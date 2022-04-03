33613e25470dc97eec2f118185a0b1dbf7bba163
juo <hwgiwh271@gmail.com>
2022-04-03 19:36:06 +0900

    [Feat] #4 App 생성
    -

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
 'use strict';
+import * as util from './utils/util.js';
+
+const homeView = class {};
+const homeVm = class {};
+
+const App = class extends Map {
+  constructor(_parent) {
+    super();
+    util.prop(this, { _parent });
+  }
+  add(path, view, vm) {
+    super.set(path, [view, vm]);
+  }
+  route(path, ...arg) {
+    const [p, action = 'base'] = [path];
+    const [view, vm] = super.get(p).map((f) => f());
+    view._homeVm = vm;
+    vm[action](...arg);
+    util.append(util.el(util.sel(this._parent), 'innerHTML', ''), view.view);
+  }
+};
+const app = new App('.App');
+app.add(
+  'home',
+  () => new homeView(true),
+  () => new homeVm(true),
+);
+app.route('home');
