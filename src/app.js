207d729bf643279ea3cc1b53f2b030e7cd6205be
juo <hwgiwh271@gmail.com>
2022-04-03 23:15:54 +0900

    [Feat] #7 뷰 생성
    -

--- src/app.js
+++ src/app.js
@@ -1,8 +1,70 @@
 'use strict';
 import * as util from './utils/util.js';
 
-const homeView = class {};
-const homeVm = class {};
+const Observer = class {
+  observe() {}
+};
+const View = class extends Observer {
+  constructor(html) {
+    super();
+    util.prop(this, { _view: html });
+  }
+  render() {
+    util.override();
+  }
+  observe() {
+    this.render();
+  }
+  get view() {
+    return this._view;
+  }
+  get viewModel() {
+    return this._vm;
+  }
+  set viewModel(_vm) {
+    util.prop(this, { _vm });
+    vm.addView(this);
+  }
+};
+const HomeView = class extends View {
+  constructor() {
+    super(document.createDocumentFragment());
+    const breadCrumb = util.sel('.Breadcrumb');
+    const nodes = util.sel('.Nodes');
+    util.prop(this, { breadCrumb, nodes });
+  }
+  render() {
+    const arr = [{ name: 'kim' }, { name: 'kim2' }, { name: 'kim3' }];
+    const { view } = this;
+    const template = document.createElement('template');
+    const nodes = arr
+      .map(
+        (obj) => `
+        <div class="Node"> 
+            <div class="div-fas"><i class="fas fa-folder-open"></i></div>
+            <div>2021/04</div>
+        </div>`,
+      )
+      .join('');
+
+    template.innerHTML = ` 
+    <nav class="Breadcrumb">
+        <div>root</div>
+        <div>노란고양이</div>
+    </nav>
+    <div class="Nodes">
+    ${nodes}
+    </div>
+    `;
+    view.appendChild(template.content);
+    setTimeout(() => {
+      util.sel('.Nodes').addEventListener('click', () => console.log(1));
+    }, 0);
+  }
+};
+const HomeVm = class {
+  base() {}
+};
 
 const App = class extends Map {
   constructor(_parent) {
@@ -17,13 +79,14 @@ const App = class extends Map {
     const [view, vm] = super.get(p).map((f) => f());
     view._homeVm = vm;
     vm[action](...arg);
+    view.render();
     util.append(util.el(util.sel(this._parent), 'innerHTML', ''), view.view);
   }
 };
 const app = new App('.App');
 app.add(
   'home',
-  () => new homeView(true),
-  () => new homeVm(true),
+  () => new HomeView(true),
+  () => new HomeVm(true),
 );
 app.route('home');
