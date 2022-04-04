0928398801b4b18c72871f5da76ac5bdd54add8f
juo <hwgiwh271@gmail.com>
2022-04-04 18:33:48 +0900

    [Feat] #2  뒤로가기 기능 구현
    -

--- src/app.js
+++ src/app.js
@@ -53,8 +53,35 @@ const HomeModel = class extends Model {
     ).json();
     util.prop(this, { _list });
   }
+  setList(_list) {
+    util.prop(this, { _list });
+  }
   get list() {
     return this._list;
+  }
+};
+const BackModel = class extends Model {
+  constructor(isSingleTon) {
+    super(isSingleTon);
+    if (!this._prevList) util.prop(this, { _prevList: [], _prevState: false });
+  }
+  save(homeModel) {
+    if (!util.is(homeModel, HomeModel)) util.err(`invalid home model ${homeModel}`);
+    this._prevList.push(homeModel.list);
+  }
+  load() {
+    const target = this._prevList.pop();
+    if (this._prevList.length === 0) util.prop(this, { _prevState: false });
+    return target;
+  }
+  setState() {
+    util.prop(this, { _prevState: true });
+  }
+  get state() {
+    return this._prevState;
+  }
+  get list() {
+    return this._prevList[this._prevList.length - 1];
   }
 };
 
@@ -86,20 +113,41 @@ const ViewModel = class extends Subject {
   }
 };
 const HomeVm = class extends ViewModel {
+  _state = false;
   base() {
     const model = new HomeModel(true);
+    const Bmodel = new BackModel(true);
     model.add(this.observer);
+    Bmodel.add(this.observer);
     model.notify();
   }
   listen(model) {
-    if (!util.is(model, HomeModel)) util.err(`invalid model : ${model}`);
-    this.notify(model.list);
+    switch (true) {
+      case util.is(model, HomeModel):
+        this.notify(model.list);
+        break;
+      case util.is(model, BackModel):
+        const target = model.load();
+        util.prop(this, { _state: model.state });
+        this.notify(target);
+        break;
+    }
   }
   async $direct(id) {
     if (!id) return;
     const homeModel = new HomeModel(true);
+    const backModel = new BackModel(true);
+    backModel.save(homeModel);
+    backModel.setState();
+    util.prop(this, { _state: backModel.state });
     await homeModel.load(id);
     homeModel.notify();
+  }
+  $prev() {
+    const homeModel = new HomeModel(true);
+    const backModel = new BackModel(true);
+    homeModel.setList(backModel.list);
+    backModel.notify();
   }
 };
 
@@ -151,12 +199,18 @@ const HomeView = class extends View {
         <div>root</div>
     </nav>
     <div class="Nodes">
+      <div class="Node  ${viewModel._state ? 'visible' : 'invisible'}">
+        <div class="div-fas">
+          <i class="fas fa-caret-left"></i>
+        </div>
+      </div>
     ${nodes}
     </div>
     `;
     util.append(util.el(view, 'innerHTML', ''), template.content);
     setTimeout(() => {
       util.sel('.Nodes').addEventListener('click', (e) => viewModel.$direct(e.target.dataset.id));
+      util.sel('.div-fas').addEventListener('click', (e) => viewModel.$prev());
     }, 0);
   }
 };
